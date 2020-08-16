538752241ffb8aa4f198406a50d26d7598115c82
Ben Johnson-Laird <rjohnsonlaird@etsy.com>
2020-08-16 21:18:04 +0000

    added check for running as root user

--- SBIR_regression/bin/runJupyter.sh
+++ SBIR_regression/bin/runJupyter.sh
@@ -1,6 +1,13 @@
-!/bin/bash
+#!/bin/bash -x
 
-pip install jupyter
+user=$USER
+
+if [ "$user" != "root" ]; then
+    echo "Must be root in docker to run this command, exiting..."
+    exit 1
+fi
+
+pip install jupyter >& /dev/null
 cd /root/SBIR_regression
 jupyter notebook --NotebookApp.token='' --port=8081 --ip=0.0.0.0 --allow-root --no-browser . &
 
