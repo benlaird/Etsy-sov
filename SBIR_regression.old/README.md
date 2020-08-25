913cde8a850a5756b07d0e0d059d12ef629e5747
Aviva Mazurek <smazurek@etsy.com>
2020-08-25 20:41:01 +0000

    new_stuff

---
+++ SBIR_regression.old/README.md
@@ -0,0 +1,28 @@
+# SBIR regression
+This repo contains code for the C&G paper "[Sketching out the details: Sketch-based image retrieval using convolutional neural networks with multi-stage regression](https://doi.org/10.1016/j.cag.2017.12.006)" 
+
+## Dependencies
+You will need to compile [Caffe v1.0](https://github.com/BVLC/caffe) with customized L2 normalize layer. Check [caffe_utils/README.md](caffe_utils/README.md) for instructions.
+
+Alternatively, you can use standard Caffe, just remove the normalize layer in model/*.prototxt, then normalise the output manually using e.g. numpy.
+
+## Pretrained model
+Pretrained model (and dataset) can be downloaded [here](http://www.cvssp.org/data/Flickr25K/cag17.html).
+
+## Feature extraction
+
+Check [getfeat_img.py](getfeat_img.py) and [getfeat_skt.py](getfeat_skt.py) for examples of extracting features from a raw image/sketch.
+
+## Demo
+[![Youtube demo](http://img.youtube.com/vi/3FRDpNk4w4U/0.jpg)](https://youtu.be/3FRDpNk4w4U)
+
+## Reference
+```
+@article{bui2018sketching,
+  title={Sketching out the Details: Sketch-based Image Retrieval using Convolutional Neural Networks with Multi-stage Regression},
+  author={Bui, Tu and Ribeiro, Leonardo and Ponti, Moacir and Collomosse, John},
+  journal={Computers \& Graphics},
+  year={2018},
+  publisher={Elsevier}
+}
+```
--- SBIR_regression/getfeat_img.py
+++ SBIR_regression/getfeat_img.py
@@ -16,8 +16,11 @@ import numpy as np
 from sklearn.metrics import confusion_matrix
 from scipy.io import savemat
 import time
+from os import listdir
+from os.path import isfile, join
+import os
 
-MODEL_WEIGHTS_PATH = 'triplet1_InceptionV1_InceptionV1_halfshare_inception4e_ld256_triplet_sketchy_iter_31200.caffemodel'
+MODEL_WEIGHTS_PATH = 'model/triplet1_InceptionV1_InceptionV1_halfshare_inception4e_ld256_triplet_sketchy_iter_31200.caffemodel'
 MODEL_SPEC_PATH = 'model/deploy_images_net1_InceptionV1_InceptionV1_halfshare_inception4e_ld256_triplet_sketchy.prototxt'
 
 
@@ -88,8 +91,23 @@ def extractitem(net, mean_pixel, fname):
     
     return prediction
 
+onlyfiles = [f for f in listdir('test_images') if isfile(join('test_images', f))]
 
+<<<<<<< HEAD
 if __name__ == "__main__":
     net = get_net(MODEL_WEIGHTS_PATH, MODEL_SPEC_PATH)
     sample_img = 'samples/airplane.png'
     feat = extractitem(net, mean_pixel, sample_img)
+    print(type(feat))
+    print('RIGHT ABOVE IS TYPE')
+    np.save('feat',feat)
+=======
+for file in onlyfiles:
+    if __name__ == "__main__":
+        net = get_net(MODEL_WEIGHTS_PATH, MODEL_SPEC_PATH)
+        sample_img = f'test_images/{file}'
+        feat = extractitem(net, mean_pixel, sample_img)
+        print(type(feat))
+        print('RIGHT ABOVE IS TYPE')
+        np.save(os.path.join('test_images',f'{file}_np'), arr)
+>>>>>>> 6ad13586b4350e938d9aca64cb11c76406a1bce7
