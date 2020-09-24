8a7a46d0cdaef700f92f92968bf65c7d25813f81
Ben Johnson-Laird <rjohnsonlaird@etsy.com>
2020-09-24 11:51:00 -0400

    added canvas to outline

--- etsyOutlineDemo/outline.py
+++ etsyOutlineDemo/outline.py
@@ -2,9 +2,15 @@ class Outline:
     id = 1
     clusters = {}
     frame = None
+    canvas = None
 
     def __init__(self):
         pass
+
+    # Reset the scrollbar to the top, assumes the parent widget is a canvas with a scrollbar
+    @classmethod
+    def scroll_to_top(cls):
+        Outline.canvas.yview_moveto('0.0')
 
     @classmethod
     def set_clusters(cls, images_clusters):
@@ -21,6 +27,14 @@ class Outline:
     @classmethod
     def get_frame(cls):
         return Outline.frame
+
+    @classmethod
+    def set_canvas(cls, canvas):
+        Outline.canvas = canvas
+
+    @classmethod
+    def get_canvas(cls):
+        return Outline.canvas
 
     def __repr__(self):
         return self.__str__()
